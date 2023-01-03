34206681ac12e78c840825f32fa5bf5557acde30
huangqiting <1787945215@qq.com>
2023-01-03 15:28:17 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <!-- 路由挂载点 -->
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {};
+  },
+  methods: {
+    aa() {},
+  },
+};
+</script>
